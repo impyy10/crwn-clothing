a98186d69f101045d16fc652a3754daa4b3acae7
impyy10 <63258139+impyy10@users.noreply.github.com>
2021-06-09 07:19:35 -0400

    added menu-item and directory components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./styles.css";
-import HomePage from './components/Homepage/homepage'
+import HomePage from './components/pages/Homepage/homepage'
 
 export default function App() {
   return (
     <div>
-      <h1>GITHUB</h1>
+      
       <HomePage />
     </div>
   );
