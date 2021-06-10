d1a11aa81ee83f4c085d251f4ebf3a7e25fa8351
impyy10 <63258139+impyy10@users.noreply.github.com>
2021-06-10 18:12:50 -0400

    added header/navbar

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import "./styles.css";
 import { Route, Switch } from 'react-router-dom'
 import HomePage from './components/pages/Homepage/homepage'
 import ShopPage from './components/pages/shop/shop'
+import Header from './components/header/header'
 
 export default function App() {
   return (
     <div>
+      <Header />
+
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
