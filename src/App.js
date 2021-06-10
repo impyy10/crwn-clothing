8ec2390ac57b08b037da704fbcb947876bc8335e
impyy10 <63258139+impyy10@users.noreply.github.com>
2021-06-10 07:11:26 -0400

    added shop page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import "./styles.css";
 import { Route, Switch } from 'react-router-dom'
 import HomePage from './components/pages/Homepage/homepage'
+import ShopPage from './components/pages/shop/shop'
 
 export default function App() {
   return (
     <div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop' component={ShopPage} />
+      </Switch>
       
-      <HomePage />
+     
     </div>
   );
 }
